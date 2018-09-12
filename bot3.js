b62947d6da1819532fc7ad4c4d7c5c8e7f01d153
salamri <42224359+salamri@users.noreply.github.com>
2018-09-12 13:06:41 +0300

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -7,13 +7,13 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = '1s';
+const prefix = 'sh';
 client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
 });
 
       client.on('ready', () => {
-              client.user.setActivity("1splay | .Fun", {type: 'LISTENING'});
+              client.user.setActivity("shplay | .Fun", {type: 'LISTENING'});
 
       });
 /*
